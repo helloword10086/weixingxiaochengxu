5c02fafb49db6f3ad58af4e14f40cc1340d3b2f9
helloword10086 <941129839@qq.com>
2019-06-18 09:38:21 +0800

    n

---
+++ colleege/src/components/list.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <h1 class="list-title">为你推荐</h1>
+    <div class="alllist">
+     <div class="mylist">
+       <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3208326311,3261429607&fm=27&gp=0.jpg" alt="" class="my">
+       <h2>领导力原则</h2>
+       <div class="all">
+
+       <p class="jieshao">俞敏洪</p>
+       </div>
+       <div class="all">
+       <p class="kan">85万人次</p>
+        </div>
+        <div class="all">
+       <view class="content">团队管理</view>
+       </div>
+     </div>
+     <div class="mylist">
+       <img src="https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=3208326311,3261429607&fm=27&gp=0.jpg" alt="" class="my">
+       <h2>领导力原则</h2>
+       <div class="all">
+
+       <p class="jieshao">俞敏洪</p>
+       </div>
+       <div class="all">
+       <p class="kan">85万人次</p>
+        </div>
+        <div class="all">
+       <view class="content">团队管理</view>
+       </div>
+     </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style scoped>
+.mylist{
+  overflow: hidden;
+  border-bottom: 1px solid #D6D8DB;
+}
+h1{
+  display: block;
+  padding: 20rpx;
+  font-weight: bold;
+  font-size: 43rpx;
+}
+
+.my{
+  display: block;
+  height: 200rpx;
+  width: 150rpx;
+  float: left;
+  border-radius: 2px;
+  margin: 20rpx;
+}
+h2{
+  display: block;
+  margin-top: 20rpx;
+  font-weight: bold;
+  font-size: 32rpx;
+  margin-bottom: 10rpx;
+}
+.jieshao{
+  font-size: 14px;
+  color: #747474;
+  margin-bottom: 10rpx;
+  margin-right: 10px;
+  float: left;
+}
+.kan{
+  font-size: 12px;
+  color: #747474;
+  margin-bottom: 10rpx;
+  margin-right: 10px;
+  float: left;
+}
+.content{
+  display: block;
+  background: #F9F2EA;
+   font-size: 12px;
+  color: #747474;
+  margin-bottom: 10rpx;
+  margin-right: 10px;
+  padding: 8rpx;
+  /* width: 10rpx; */
+  float: left;
+}
+.all{
+  overflow: hidden;
+}
+</style>
---
+++ colleege/src/pages/main/index.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="header">
+    <img src="../../../static/images/head.png" alt="" class="header-img">
+    <div class="tui-jian">
+      <img src="../../../static/images/tui.png" alt="" class="tuijian">
+    </div>
+    <div class="all-list">
+
+     <list/>
+    </div>
+     <div class="foot">
+       <div class="foot-content"> 更多精彩课程，就在"混沌大学"APP</div>
+     </div>
+  </div>
+  
+</template>
+<script>
+import list from '@/components/list'
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  components:{list}
+}
+</script>
+<style >
+page{
+  background: #EDEDED;
+  height: 100%;
+  position: relative;
+}
+.header{
+  height: 100%;
+}
+.header-img{
+  width: 100%;
+  height: 277rpx;
+}
+.tuijian{
+  display: block;
+  width: 95%;
+  height: 200rpx; 
+  margin: 10rpx auto;
+}
+.all-list{
+  background: #fff;
+}
+.foot{
+  position: absolute;
+  /* top: 100px; */
+  /* position: flex; */
+  background: #3F4244;
+top: 1125rpx;
+  color: #BB9868;
+  width: 100%;
+}
+.foot-content{
+  display: block;
+  margin: 10px auto;
+ /* justify-content: center; */
+ text-align: center;
+ 
+}
+</style>
